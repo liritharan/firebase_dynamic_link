dafcdca58e668e944931d08217f7ef7f50824ad8
liritharan <lirininfo@gmail.com>
2023-02-16 18:15:59 +0530

    Adding dynamic links

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:share_plus/share_plus.dart';
+import 'option.dart';
+
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Get any initial links
+
   runApp(const MyApp());
 }
 
@@ -13,103 +26,180 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomePage(),
+        '/helloworld': (BuildContext context) => _DynamicLinkScreen(),
+      },
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  final TextEditingController _editingController = TextEditingController();
+  FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
+  final String DynamicLink = 'https://www.google.com/helloworld';
+  final String Link = 'https://referandearnra.page.link/XktS';
+  late Timer _timerLink;
+  bool _isCreatingLink = false;
+  String? _linkMessage;
+@override
+void initState(){
+  initDynamicLinks();
+  super.initState();
+}
+  Future<void> initDynamicLinks() async {
+    dynamicLinks.onLink.listen((dynamicLinkData) {
+      print('app is here');
+      Navigator.pushNamed(context,'/helloworld' );
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+    }).onError((error) {
+      print('onLink error');
+      print(error.message);
+    });
+  }
+  Future<void> _createDynamicLink(bool short) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isCreatingLink = true;
+    });
+
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://referandearnra.page.link/XktS',
+      longDynamicLink: Uri.parse(
+        'https://referandearnra.page.link/?link=https://www.google.com&apn=com.example.firebase_dynamic_link',
+      ),
+      link: Uri.parse(DynamicLink),
+      androidParameters: const AndroidParameters(
+        packageName: 'com.example.firebase_dynamic_link',
+        minimumVersion: 0,
+      ),
+      iosParameters: const IOSParameters(
+        bundleId: 'com.example.firebase_dynamic_link',
+        minimumVersion: '0',
+      ),
+    );
+
+    Uri url;
+    if (short) {
+      final ShortDynamicLink shortLink =
+      await dynamicLinks.buildShortLink(parameters);
+      url = shortLink.shortUrl;
+      print(url);
+    } else {
+      url = await dynamicLinks.buildLink(parameters);
+      print(url);
+    }
+
+    setState(() {
+      _linkMessage = url.toString();
+      _isCreatingLink = false;
     });
   }
 
+
+  @override
+
+
+  @override
+  void dispose() {
+    _timerLink.cancel();
+    super.dispose();
+  }
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Deep Linking"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _editingController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter Text To Share",
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final PendingDynamicLinkData? data =
+              await dynamicLinks.getInitialLink();
+              final Uri? deepLink = data?.link;
+
+              if (deepLink != null) {
+                // ignore: unawaited_futures
+                Navigator.pushNamed(context, deepLink.path);
+              }
+            },
+            child: const Text('getInitialLink'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final PendingDynamicLinkData? data =
+              await dynamicLinks
+                  .getDynamicLink(Uri.parse(Link));
+              final Uri? deepLink = data?.link;
+
+              if (deepLink != null) {
+                // ignore: unawaited_futures
+                Navigator.pushNamed(context, deepLink.path);
+              }
+            },
+            child: const Text('getDynamicLink'),
+          ),
+          ElevatedButton(
+            onPressed: !_isCreatingLink
+                ? () => _createDynamicLink(false)
+                : null,
+            child: const Text('Get Long Link'),
+          ),
+          ElevatedButton(
+            onPressed: !_isCreatingLink
+                ? () {
+              _createDynamicLink(true);
+
+            }
+                : null,
+            child: const Text('Get Short Link'),
+          ),
+
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+
+
     );
   }
 }
+
+class _DynamicLinkScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World DeepLink'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }}
